e894555058e25c1ad52671f78d8d08f39f8a4690
zterhes <zterhes1@gmail.com>
2021-03-08 22:59:07 +0100

    add new section to resume page

--- src/components/styleComponents/styleHolder.jsx
+++ src/components/styleComponents/styleHolder.jsx
@@ -3,9 +3,13 @@ import backgroundPic from '../../img/old-black-background-grunge-texture-dark-wa
 
 export const ContentDiv = styled.div`
 display:flex;
+flex-direction:column;
+justify-content:space-evenly;
+align-items:center;
 background:url(${backgroundPic});
 height: 100vh;
 width:100vw;
+color:white;
 clip-path: polygon(0 0, 100% 6%, 100% 100%, 0 94%);
 `
 
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from "styled-components";
 import backgroundPic from "../img/chip.jpg"
 import { useTranslation } from "react-i18next";
-import Page from "../components/styleComponents/Page";
+import PageStyle from "../components/styleComponents/Page";
 import ShadowText from '../components/styleComponents/shadowText'
 import { ContentDiv } from '../components/styleComponents/styleHolder'
+import diplom from '../img/oklevel.jpg'
 
 
 const TitleDiv = styled.div`
@@ -16,27 +17,79 @@ justify-content:center;
 align-items:center;
 `
 
-const Title = styled.h1`
-font-size:10rem;
+const Img = styled.img`
+height:50vh;
+`
+
+const SmallTitle = styled.p`
+font-size:30px;
+font-weight:bold;
+`
+
+const ContentInlineDiv = styled.div`
+display:flex;
+justify-content:space-between;
 `
 
 
+
 const Resume = () => {
+    const [age, setAge] = useState()
     const { t } = useTranslation();
+
+    const calculateAge = () => {
+        const bday = new Date("1990-01-17");
+        const ageDif = new Date(Date.now() - bday)
+        setAge(ageDif.getUTCFullYear() - 1970)
+    }
+
+    useEffect(() => {
+        calculateAge()
+    })
+
+
     return (
-        <Page url={backgroundPic}>
+        <PageStyle url={backgroundPic}>
             <TitleDiv>
-                <ShadowText textSize="10rem">{t("resume.profile")}</ShadowText>
+                <ShadowText textSize="10rem">{t("profile")}</ShadowText>
             </TitleDiv>
             <div>
                 <ContentDiv>
-                    <h1>test</h1>
+                    <div>
+                        <h1>{t("terheszoran")}</h1>
+                    </div>
+                    <div className="text-center">
+                        <h3>{t("age")}</h3>
+                        <h3>{age}</h3>
+                    </div>
+                    <div className="text-center">
+                        <h3>{t("location")}</h3>
+                        <h3>{t("locationData")}</h3>
+                    </div>
+                    <div className="text-center">
+                        <h3>{t("text")}</h3>
+                    </div>
                 </ContentDiv>
             </div>
             <TitleDiv>
-                <ShadowText textSize="10rem">{t("resume.educations")}</ShadowText>
+                <ShadowText textSize="10rem">{t("educations")}</ShadowText>
             </TitleDiv>
-        </Page>
+            <div>
+                <ContentDiv>
+                    <div className="text-center">
+                        <h1>{t("juniorfrontenddeveloper")}</h1>
+                        <h2>Flow Academy Szeged</h2>
+                        <h2>2020-2021</h2>
+                    </div>
+                    <ContentInlineDiv>
+                        <div>
+                            <SmallTitle>5 hónapos 800 órás Full Stack Webfejlesztő képzés</SmallTitle>
+                        </div>
+                        <Img src={diplom} />
+                    </ContentInlineDiv>
+                </ContentDiv>
+            </div>
+        </PageStyle>
 
     );
 }
